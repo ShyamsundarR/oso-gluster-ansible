3a44b3b3c9f4c6bea143a941442d4b5297d51372
John Strunk <jstrunk@redhat.com>
2018-05-19 15:57:06 -0400

    Properly probe for gluster metrics from aggregator
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- roles/pcp-aggregator/files/pcp2zabbix_with_gluster.sh
+++ roles/pcp-aggregator/files/pcp2zabbix_with_gluster.sh
@@ -1,6 +1,23 @@
 #! /bin/bash
 
 # The purpose of this wrapper script is to allow somw hosts to export gluster
-# metrics while others don't. So, we first try with, and if that fails, without.
+# metrics while others don't. So, we probe for the gluster metrics and use
+# the result to choose
 
-pcp2zabbix "$@" gluster || pcp2zabbix "$@"
+function gethost {
+    while [[ $# -gt 0 ]]; do
+        if [ "$1" == "-h" ]; then
+            echo "$2"
+            return
+        fi
+        shift
+    done
+}
+
+remotehost=$(gethost "$@")
+
+if pminfo -h "$remotehost" gluster >& /dev/null; then
+    pcp2zabbix "$@" gluster
+else
+    pcp2zabbix "$@"
+fi
